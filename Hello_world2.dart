dcf61bc1363fb91b3d3b4a190387dc1b3bc9abdc
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-18 23:08:34 +0530

    Create Hello_world2.dart

---
+++ Hello_world2.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:const Center(
+      child:Text('Hello World',
+      textDirection:TextDirection.rtl,
+      ),
+    ),
+     ),);
+
+}
