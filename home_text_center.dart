b1a8c136c79995dde380f4e6bf152e0eab8edf4f
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-18 10:12:28 +0530

    Create home_text_center.dart

---
+++ home_text_center.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp (home:Center(child:Text('Welcome TO Firstly'),),),); //center and child is used to place text in center
+}
