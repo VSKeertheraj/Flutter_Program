df75c73cfa8b66e250aec05802a4778f9944e4a4
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-18 10:50:23 +0530

    Create title_and_body.dart

---
+++ title_and_body.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp (
+    home:Scaffold(
+      appBar:AppBar(
+        title:Center(child:Text('Welcome TO Firstly'),
+        ), //title Center
+        ), //AppBar
+        body:Center(child:Text('New To Flutter'),
+        ), // Body Center
+        ), //  Scaffold
+        ), //MaterialApp
+        );
+}
