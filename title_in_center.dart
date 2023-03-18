036d11816dd3db34d0886d67bd0adab28a1c48b9
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-18 10:43:33 +0530

    Create title_in_center.dart

---
+++ title_in_center.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp (
+    home:Scaffold(
+      appBar:AppBar(
+        title:Center(child:Text('Welcome TO Firstly'),
+        ), //Center
+        ), //AppBar
+        ), //Scaffold
+        ), //MaterialApp
+        ); //runApp
+}
