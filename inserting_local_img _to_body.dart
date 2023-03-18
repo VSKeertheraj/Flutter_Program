bc80fa44123015d0b47548f45ef31df6fd182b6e
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-18 13:30:10 +0530

    Create inserting_local_img _to_body.dart

---
+++ inserting_local_img _to_body.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp (
+    home:Scaffold(
+      appBar:AppBar(
+        title:const Center(child:Text('Welcome To Firstly'),
+        ), //Title Center
+        backgroundColor: Colors.pink[400], // there are many shades in colors so visit flutter .docs for color code 
+        ), //AppBar
+        body:const Center(
+          child:Image(
+          image:
+          AssetImage('images/geek.jpg')),
+        ), //Body Center
+      backgroundColor: Colors.pink[200]
+        ), //Scaffold
+        ), //MaterialApp
+        );
+}
