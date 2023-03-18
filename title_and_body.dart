841778049db869f1a90899c6614ef36d33aaa917
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-18 18:34:25 +0530

    Update title_and_body.dart

--- title_and_body.dart
+++ title_and_body.dart
@@ -4,10 +4,12 @@ void main() {
   runApp( MaterialApp (
     home:Scaffold(
       appBar:AppBar(
-        title:Center(child:Text('Welcome TO Firstly'),
+        title:Center(
+          child:Text('Welcome TO Firstly'),
         ), //title Center
         ), //AppBar
-        body:Center(child:Text('New To Flutter'),
+        body:Center(
+          child:Text('New To Flutter'),
         ), // Body Center
         ), //  Scaffold
         ), //MaterialApp
