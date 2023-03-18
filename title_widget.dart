cca071fd7bd6f8cda1901bc272f0e956d65161f4
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-18 10:39:47 +0530

    Update title_widget.dart

--- title_widget.dart
+++ title_widget.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp (home:Scaffold(appBar:AppBar(title:Text('Welcome Geek!')))));
+  runApp( MaterialApp (
+    home:Scaffold(
+      appBar:AppBar(
+        title:Text('Welcome Geek!'),
+        ), // AppBar
+        ), //Scaffold
+        ), //MaterialApp
+        );
 }
