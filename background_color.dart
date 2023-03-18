5097acfe954d0fad4c59ed332b3a0f3191fdec21
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-18 12:10:21 +0530

    Update background_color.dart

--- background_color.dart
+++ background_color.dart
@@ -6,10 +6,11 @@ void main() {
       appBar:AppBar(
         title:Center(child:Text('Welcome To Firstly'),
         ), // title Center
-        backgroundColor: Colors.pink[200], // there are many shades in colors so visit flutter .docs for color code 
+        backgroundColor: Colors.pink[400], // there are many shades in colors so visit flutter .docs for color code 
         ), // appBar
         body:Center(child:Text('New To Flutter'),
         ), // body Center
+      backgroundColor: Colors.pink[100]
         ), // Scaffold
         ), // MaterialApp
         );
