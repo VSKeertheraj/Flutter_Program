a847618cbb3b7b387d9b50ecffa67d77dce891ed
Keertheraj <121630418+VSKeertheraj@users.noreply.github.com>
2023-03-18 17:49:35 +0530

    Create container.dart

---
+++ container.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyWidget());
+}
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp (
+    home:Scaffold(
+      appBar:AppBar(
+        title:const Center(child:Text('Welcome To Firstly'),
+        ),
+        backgroundColor: Color.fromARGB(255, 67, 10, 29), // there are many shades in colors so visit flutter .docs for color code 
+        ), 
+
+        body:Container(
+          height:200.0,
+          width:200.0,
+          color:Colors.amberAccent,
+          alignment:Alignment.center,
+          padding:const EdgeInsets.all(20.0),
+          child:Text('Hello'),
+        ),
+        ),
+        );
+  }
+}
